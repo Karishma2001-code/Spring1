5b4d9d3c3fbb040f8181ae269d2729194c9d2125
Karishma2001-code <karishma.3349.choudhary@gmail.com>
2025-04-30 03:19:31 +0530

    Add files via upload

---
+++ by using xml/Student.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name      : " + name);
+        System.out.println("Course    : " + course);
+    }
+}
