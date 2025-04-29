7da87b7c1d5abcb3b96f217192d5f7f85847800c
Karishma2001-code <karishma.3349.choudhary@gmail.com>
2025-04-30 04:05:35 +0530

    Add files via upload

---
+++ autowired annotation use/AppConfig.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Address address(){
+        Address a=new Address(101,"burhanpur",450331);
+        return  a;
+    }
+    @Bean
+    public Student stu1() {
+        Student s = new Student(1,"Amruta","java");
+        return s;
+    }
+}
---
+++ autowired annotation use/Student.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+    @Autowired
+    private Address address;
+    public Student(int id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+    public void displayInfo() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name      : " + name);
+        System.out.println("Course    : " + course);
+        System.out.println("Address    : " + address);
+    }
+}
