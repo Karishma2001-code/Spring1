5d6790c1634bf246554e06915b1fa57d3d10d804
Karishma2001-code <karishma.3349.choudhary@gmail.com>
2025-04-30 04:26:50 +0530

    Add files via upload

---
+++ prototype/AppConfig.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Address address1(){
+        Address a=new Address(101,"burhanpur",450331);
+        return  a;
+    }
+    @Bean
+    public Address address2(){
+        Address a=new Address(102,"pune",450331);
+        return  a;
+    }
+    @Bean
+    @Scope("prototype")
+    public Student stu1() {
+        Student s = new Student(1,"Amruta","java");
+        return s;
+    }
+}
